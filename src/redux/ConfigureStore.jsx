e4ef6ac37be2d3071785c48b376edcfa3ca7845c
Raduev <dominator95rrr@gmail.com>
2022-01-19 14:55:29 +0300

    added combine

--- src/redux/ConfigureStore.jsx
+++ src/redux/ConfigureStore.jsx
@@ -2,8 +2,9 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { logger } from 'redux-logger/src';
+import { turs } from "./features/searchReducer"
 
-const combineReducer = combineReducers({});
+const combineReducer = combineReducers({turs});
 
 const store = createStore(combineReducer, composeWithDevTools(applyMiddleware(thunk, logger)));
 
