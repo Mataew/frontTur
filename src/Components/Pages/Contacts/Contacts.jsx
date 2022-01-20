f6616460de371211e100db416f66739cd5f3dce8
Husein <ahmadov.m48@gmail.com>
2022-01-20 12:34:08 +0300

    начал делать компонент контакты

--- src/Components/Pages/Contacts/Contacts.jsx
+++ src/Components/Pages/Contacts/Contacts.jsx
@@ -5,18 +5,31 @@ import styles from './contacts.module.css'
 const Contacts = () => {
   return (
     <div className={styles.mainBlock}>
-      <div className={styles.numbers}>
-        <h1>Контактная информация для частных лиц</h1>
-        <h2>По всем интересующим вопросам обращайтесь по телефонам:</h2>
-        <p>+7 (495) 232 10 11 - колл-центр Грозный</p>
-        <p>+7 (495) 232 08 09 - горячая линия для туристов 24/7</p>
-        <p>Cлужба поддержки и продаж OTI Hotels&Resorts для клиентов из России: + 7 (800) 444 24 11</p>
+      <div className={styles.contactUs}>
+        <div className={styles.contactUsText}>
+          <h1>Свяжитесь с нами</h1>
+          <p>Хотите ли вы связаться с краткой информацией или просто узнать, как добраться до офиса, вы можете найти всю необходимую информацию ниже.</p>
+        </div>
+        <div className={styles.contactUsFirstImage}>
+        <img src="https://d2jd1xj0qdoqs5.cloudfront.net/craft/uploads/Contact/Contact-Left-Half.jpg?mtime=20190213101134" />
+        </div>
+        <div className={styles.contactUsSecondImage}>
+        <img src="https://d2jd1xj0qdoqs5.cloudfront.net/craft/uploads/Contact/Contact-Right-Half.jpg?mtime=20190213101135" />
+        </div>
       </div>
-      <div className={styles.map}>
-        <h3>Офис продаж:</h3>
-        <p>г. Грозный, ул. Трошева, д. 7</p>
-        <h3>Режим работы офиса</h3>
-        
+      <div className={styles.findUs}>
+        <div className={styles.findUsAdress}>
+          <h1>Как нас найти</h1>
+          <p>г. Грозный, ул. Трошева, дом 7</p>
+          <a href="">Показать на карте</a>
+        </div>
+        <div className={styles.findUsGetInTouch}>
+          <h1>Как с нами связаться</h1>
+            <p>Наша почта</p>
+            <p>vaytur@mail.ru</p>
+            <p>Номер для связи</p>
+            <p>+7(999)-999-99-99</p>
+        </div>
       </div>
     </div>
   );
