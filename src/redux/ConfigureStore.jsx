b6f6b2f3b3d60a8208125d9bacd7b64fe9d706a2
Mamdi <masuev12@gmail.com>
2022-01-20 11:26:02 +0300

    добавит редюсер регистрации в комбайн

--- src/redux/ConfigureStore.jsx
+++ src/redux/ConfigureStore.jsx
@@ -2,8 +2,11 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { logger } from 'redux-logger/src';
+import authorizatonReducer from './reducerAuthorization'
 
-const combineReducer = combineReducers({});
+const combineReducer = combineReducers({
+    authorizatonReducer
+});
 
 const store = createStore(combineReducer, composeWithDevTools(applyMiddleware(thunk, logger)));
 
