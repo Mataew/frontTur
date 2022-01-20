a81deed6c6360ff1e093362ea8f6908ee00cfd8f
Ibragim <mih230903@gmail.com>
2022-01-20 12:30:50 +0300

    profile and cart

--- src/Components/Pages/Profile/Profile.jsx
+++ src/Components/Pages/Profile/Profile.jsx
@@ -4,9 +4,26 @@ import styles from './profile.module.css'
 // страница для Профиля
 const Profile = () => {
   return (
-    <div>
-      <h1>Привет я ибра</h1>
-    </div>
+<div className={styles.Profile__wrapper}>
+<div className={styles.Profile__block}>
+  <div className={styles.Profile__block__img}>
+    <img src="https://www.pngplay.com/wp-content/uploads/12/User-Avatar-Profile-PNG-Photo-Image.png" alt="" />
+  </div>
+  <div className={styles.Profile__block__name}>
+    Ибрагим Ибрагимов
+  </div>
+  <div className={styles.Profile__block__number}>
+    +7 (922)-047-27-17
+  </div>
+  <div className={styles.Profile__block__exitBtn}>
+    <button>Выход</button>
+  </div>
+</div>
+<div className={styles.Profile__cart}>
+  <h2>Мои предпочтения:</h2>
+  
+</div>
+</div>
   );
 };
 
