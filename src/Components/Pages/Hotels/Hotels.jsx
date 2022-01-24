c3422db2fcf8a3748dbc33d212048dc6d1ee76ba
halid <barzaev97@gmail.com>
2022-01-24 14:53:38 +0300

    adsd disptatch hotels

--- src/Components/Pages/Hotels/Hotels.jsx
+++ src/Components/Pages/Hotels/Hotels.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
+import {useEffect} from 'react';
+import { GetByTur } from '../../../redux/features/turDesk';
 
 const Hotels = () => {
+  const tur = useSelector((state) => state.byTurDesk.byTur)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(GetByTur())
+  },[dispatch])
+  console.log(tur,1111111)
   return (
     <div>
-      
+      {toString(tur)}
     </div>
   );
 };
