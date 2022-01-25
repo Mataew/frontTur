8014e409fe3448a0f758ca0f35888c3f66226868
Raduev <dominator95rrr@gmail.com>
2022-01-25 16:39:06 +0300

    fix

--- src/Components/Pages/Hotels/Hotels.jsx
+++ src/Components/Pages/Hotels/Hotels.jsx
@@ -3,14 +3,18 @@ import {useSelector} from 'react-redux';
 import {useDispatch} from 'react-redux';
 import {useEffect} from 'react';
 import { GetByTur } from '../../../redux/features/turDesk';
+import { useParams } from 'react-router-dom';
 
 const Hotels = () => {
-  const tur = useSelector((state) => state.byTurDesk.byTur)
+  const tur = useSelector((state) => state.byTurDesk.byTur);
+
+  const id = useParams('id');
+
   const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(GetByTur())
-  },[dispatch])
-  console.log(tur,1111111)
+    dispatch(GetByTur(id))
+  },[dispatch, id])
+  console.log(id,1111111)
   return (
     <div>
       {toString(tur)}
