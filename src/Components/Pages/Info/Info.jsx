1846ee1415ecba473e71baa74a5b213c4730b762
Husein <ahmadov.m48@gmail.com>
2022-01-26 13:16:44 +0300

    поигрался со стилями в компоненте Info

--- src/Components/Pages/Info/Info.jsx
+++ src/Components/Pages/Info/Info.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { GetTurs } from '../../../redux/features/searchReducer';
- import stylesTour from './info.module.css'
-import styles from "../Main/Search/search.module.css";
+ import styles from './info.module.css'
+
 
 // Страница "Полезная информация"
 const Info = () => {
@@ -25,24 +25,29 @@ const Info = () => {
  
   
 
-  return (
-    <div className={stylesTour.main}>
-           <div className={styles.tur_name}>
-        {rating.map((item) => {
-          return (
-            <div data-aos="img-animation" className={styles.search_imgBlock}>
-            <div className={ styles.tours_cards_wrapper }>
-            <img className={styles.tur_img} src={`http://localhost:7000/${item.img}`}  alt="daun"/>
-            </div>
-            <div className={ styles.absolute_display}>
-            <h3 className={ styles.tours_cards_title}>{item.to}</h3>
-            </div>  
+  return (<>
+  
+    <div className={styles.tours_map_img}>
+    <h1>Отдых в любой точке мира</h1>
+          <img src="https://klike.net/uploads/posts/2020-06/1592467299_7.jpg" alt="" />
+        </div>
+        <h1>Топовые предложения от топового Хусейна</h1>
+    <div className={ styles.tours_wrapper}>
+    { rating.map((item) => {
+      return (
+        <div className={ styles.tours_cards_wrapper }>
+          <img src={`http://localhost:7000/${item.img}`}  alt="daun"/>
+          <h3 className={ styles.tours_cards_title}>{item.to}</h3>
+          <div className={ styles.hover_display }>
+            <div>ОТКУДА: { item.from }</div>
+            <div>КУДА: {  item.to}</div>
+            <div>ЦЕНА: {  item.price}</div>
           </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+        </div>
+        )
+    })}
+  </div>
+  </>);
 };
 
 export default Info;
