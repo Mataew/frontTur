91c9b1998b32686c798278fb5c2df59e87aa42d2
Raduev <dominator95rrr@gmail.com>
2022-01-26 12:39:55 +0300

    fix auth

--- src/Components/Pages/Authorization/Authorization.js
+++ src/Components/Pages/Authorization/Authorization.js
@@ -11,8 +11,6 @@ const Authorization = () => {
   const [Switch, setSwitch] = useState("form-box");
   const [bodyClass, setBodyClass] = useState("body");
 
-  
-
   const Active = () => {
     setSwitch("form-box active");
     setBodyClass("body active");
@@ -23,39 +21,71 @@ const Authorization = () => {
     setBodyClass("body");
   };
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const check = useSelector(state => state.authorizatonReducer.check)
-  const signingUp = useSelector(state => state.authorizatonReducer.signingUp)
-  const err = useSelector(state => state.authorizatonReducer.error)
-  console.log(err);
-  const signingIn = useSelector(state => state.authorizatonReducer.signingIn)
-  const [login, setLogin] = useState('')
-  const [password, setPassword] = useState('')
-  const [firstName, setFirstname] = useState('')
-  const [lastName, setLastname] = useState('')
-  const [authLogin, setAuthLogin] = useState('')
-  const [authPassword, setAuthPassword] = useState('')
+  const check = useSelector((state) => state.authorizatonReducer.check);
+  const signingUp = useSelector((state) => state.authorizatonReducer.signingUp);
+  const err = useSelector((state) => state.authorizatonReducer.error);
+  const signingIn = useSelector((state) => state.authorizatonReducer.signingIn);
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstname] = useState("");
+  const [lastName, setLastname] = useState("");
+  const [authLogin, setAuthLogin] = useState("");
+  const [authPassword, setAuthPassword] = useState("");
   const [NotEmail, setNotEmail] = useState("");
+  const [NotAuthEmail, setNotAuthEmail] = useState("");
+  const [blurLogin, setBlurLogin] = useState(true);
+  const [blurPassword, setBlurPassword] = useState(true);
+  const [authBlurLogin, setAuthBlurLogin] = useState(true);
+  const [authBlurPassword, setAuthBlurPassword] = useState(true);
+  const [authBlurFirstName, setAuthBlurFirstName] = useState(true);
+  const [authBlurLastName, setAuthBlurLastName] = useState(true);
+  const [e, setE] = useState(signingIn);
+
+  const handleBlurPassword = () => {
+    password === "" ? setBlurPassword(false) : setBlurPassword(true);
+  };
+
+  const handleBlurLastName = () => {
+    lastName === "" ? setAuthBlurLastName(false) : setAuthBlurLastName(true);
+  };
+
+  const handleBlurFirstName = () => {
+    firstName === "" ? setAuthBlurFirstName(false) : setAuthBlurFirstName(true);
+  };
+
+  const handleBlurLogin = () => {
+    login === "" ? setBlurLogin(false) : setBlurLogin(true);
+  };
+
+  const handleBlurAutLogin = () => {
+    authLogin === "" ? setAuthBlurLogin(false) : setAuthBlurLogin(true);
+  };
+
+  const handleBlurAutPassword = () => {
+    authPassword === ""
+      ? setAuthBlurPassword(false)
+      : setAuthBlurPassword(true);
+  };
 
   const handleAuthLogin = (e) => {
-    setAuthLogin(e.target.value)
+    setAuthLogin(e.target.value);
     const valid =
       /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     if (!valid.test(String(e.target.value).toLowerCase())) {
-      setNotEmail("Некоректный емаил");
+      setNotAuthEmail("Некоректный емаил");
     } else {
-      setNotEmail("");
+      setNotAuthEmail("");
     }
-  }
+  };
 
   const handleAuthPassword = (e) => {
-    setAuthPassword(e.target.value)
-  }
-
+    setAuthPassword(e.target.value);
+  };
 
   const handleChangeLogin = (e) => {
-    setLogin(e.target.value)
+    setLogin(e.target.value);
     const valid =
       /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     if (!valid.test(String(e.target.value).toLowerCase())) {
@@ -63,36 +93,36 @@ const Authorization = () => {
     } else {
       setNotEmail("");
     }
-  }
+  };
 
   const handleChangePassword = (e) => {
-    setPassword(e.target.value)
-  }
+    setPassword(e.target.value);
+  };
 
   const handleChengeFirstname = (e) => {
-    setFirstname(e.target.value)
-  }
+    setFirstname(e.target.value);
+  };
   const handleChengeLastname = (e) => {
-    setLastname(e.target.value)
-  }
+    setLastname(e.target.value);
+  };
 
-  
   const handleSubmitAuth = () => {
-    dispatch(authUser(authLogin, authPassword))
-  }
+    dispatch(authUser(authLogin, authPassword));
+  };
 
-const handleSubmit = () => {
-   dispatch(createUser(login, password, firstName, lastName))
-   setLogin('')
-   setPassword('')
-   setFirstname('')
-   setLastname('')
-   if (check){
-    setSwitch("form-box");
-    setBodyClass("body");
-   }
-}
+  const handleSubmit = () => {
+    dispatch(createUser(login, password, firstName, lastName));
+    setLogin("");
+    setPassword("");
+    setFirstname("");
+    setLastname("");
+    if (check) {
+      setSwitch("form-box");
+      setBodyClass("body");
+    }
+  };
 
+  console.log()
 
   return (
     <div className={bodyClass}>
@@ -115,75 +145,110 @@ const handleSubmit = () => {
           <form action="#" className="form form_signin">
             <h3 className="form__title">Вход</h3>
             <p>
-            {!NotEmail ? <h3 className="error">{err}</h3> : <h3 className="error">{NotEmail}</h3>}
-              <input
+              {!NotAuthEmail ? (
+                <h3 className="error">{err}</h3>
+              ) : (
+                <h3 className="error">{NotAuthEmail}</h3>
+              )}
+              <TextField
                 className="form__input"
+                onBlur={handleBlurAutLogin}
+                label={!authBlurLogin ? "Поле пустое!" : "Email"}
                 placeholder="login"
                 value={authLogin}
                 onChange={handleAuthLogin}
                 type="text"
-              ></input>
+              ></TextField>
             </p>
             <p>
-              <input
+              <TextField
                 className="form__input"
                 placeholder="Password"
+                onBlur={handleBlurAutPassword}
+                label={!authBlurPassword ? "Поле пустое!" : "Password"}
                 value={authPassword}
                 onChange={handleAuthPassword}
                 type="password"
-              ></input>
+              ></TextField>
             </p>
             <p>
-              <Link to='/'
-              disabled={false}
-              onClick={handleSubmitAuth}
-              className="form__btn"> Войти</Link>
+                <button
+                  className="form__btn"
+                  disabled={
+                    !authLogin || !authPassword || signingIn || NotAuthEmail
+                  }
+                >
+                  <Link to="/" onClick={handleSubmitAuth} className="form__btn">
+                  Войти
+                  </Link>
+                </button>
             </p>
           </form>
 
           <form action="#" className="form form_signup">
             <h3 className="form__title">Регистрация</h3>
-            {!NotEmail ? <h3 className="error">{err}</h3> : <h3 className="error">{NotEmail}</h3>}
+            {!NotEmail ? (
+              <h3 className="error">{err}</h3>
+            ) : (
+              <h3 className="error">{NotEmail}</h3>
+            )}
             <p>
-              <input
+              <TextField
                 className="form__input"
                 placeholder="Email"
+                onBlur={handleBlurLogin}
+                label={!blurLogin ? "Поле пустое!" : "Email"}
                 value={login}
                 onChange={handleChangeLogin}
                 type="email"
-              ></input>
+              ></TextField>
             </p>
             <p>
-              <input
+              <TextField
                 className="form__input"
+                onBlur={handleBlurPassword}
+                label={!blurPassword ? "Поле пустое!" : "Password"}
                 placeholder="Password"
                 value={password}
                 onChange={handleChangePassword}
                 type="password"
-              ></input>
+              ></TextField>
             </p>
             <p>
-              <input
+              <TextField
                 className="form__input"
+                onBlur={handleBlurFirstName}
+                label={!authBlurFirstName ? "Поле пустое!" : "Имя"}
                 placeholder="Firstname"
                 value={firstName}
                 onChange={handleChengeFirstname}
                 type="text"
-              ></input>
-            </p>            <p>
-              <input
+              ></TextField>
+            </p>{" "}
+            <p>
+              <TextField
+                onBlur={handleBlurLastName}
+                label={!authBlurLastName ? "Поле пустое!" : "Фамилия"}
                 className="form__input"
                 placeholder="Lastname"
                 value={lastName}
                 onChange={handleChengeLastname}
                 type="text"
-              ></input>
+              ></TextField>
             </p>
             <p>
               <button
-              disabled={signingUp}
-              onClick={handleSubmit}
-              className="form__btn form__btn_signup">
+                disabled={
+                  !login ||
+                  !password ||
+                  !lastName ||
+                  !firstName ||
+                  signingUp ||
+                  NotEmail
+                }
+                onClick={handleSubmit}
+                className="form__btn form__btn_signup"
+              >
                 Зарегистрироваться
               </button>
             </p>
--- src/redux/reducerAuthorization.js
+++ src/redux/reducerAuthorization.js
@@ -2,117 +2,121 @@ const initialState = {
   signingUp: false,
   signingIn: false,
   error: null,
-  token: localStorage.getItem('token'),
-  
-  check: false
+  token: localStorage.getItem("token"),
+
+  check: false,
 };
 
-export default function auth(state = initialState, action) { 
-  switch (action.type) { 
-    case "application/signup/pending": 
-      return { 
-        ...state, 
-        signingUp: true, 
-        error: null, 
-      }; 
- 
-    case "application/signup/fulfilled": 
-      return { 
-        ...state, 
-        signingUp: false, 
-        user: action.payload ,
-        check: true
-      }; 
- 
-    case "application/signup/rejected": 
-      return { 
-        ...state, 
-        signingUp: false, 
-        error: action.error, 
-        check:false 
-      }; 
- 
-    case "application/signin/pending": 
-      return { 
-        ...state, 
-        signingIn: true, 
-        error: null, 
-      }; 
- 
-    case "application/signin/fulfilled": 
-      return { 
-        ...state, 
-        signingIn: false, 
-        token: action.payload.json.token, 
-      }; 
- 
-    case "application/signin/rejected": 
-      return { 
-        ...state, 
-        signingIn: false, 
-        error: action.error, 
-      }; 
-    case "application/logOut/fulfilled": 
-      return { 
-        ...state, 
-        token: null, 
-      }; 
-    default: 
-      return state; 
-  } 
-} 
- 
-export const createUser = (login, password, firstName, lastName) => { 
-  return async (dispatch) => { 
-    dispatch({ type: "application/signup/pending" }); 
- 
-    const responseRegister = await fetch("http://localhost:7000/users", { 
-      method: "POST", 
-      body: JSON.stringify({ login, password, firstName, lastName }), 
-      headers: { 
-        "Content-type": "application/json", 
-      }, 
-    }); 
- 
-    const jsonRegister = await responseRegister.json(); 
+export default function auth(state = initialState, action) {
+  switch (action.type) {
+    case "application/signup/pending":
+      return {
+        ...state,
+        signingUp: true,
+        error: null,
+      };
 
- 
- 
-    if (jsonRegister.error) { 
-      dispatch({ type: "application/signup/rejected", error: jsonRegister.error }); 
-    } else { 
-      dispatch({ type: "application/signup/fulfilled", payload: jsonRegister }); 
-    } 
-  }; 
-}; 
- 
-export const authUser = (login, password) => { 
-  return async (dispatch) => { 
-    dispatch({ type: "application/signin/pending" }); 
-    console.log(login);
- 
-    const response = await fetch("http://localhost:7000/login", { 
-      method: "POST", 
-      body: JSON.stringify({ login, password }), 
-      headers: { 
-        "Content-type": "application/json", 
-      }, 
-    }); 
- 
-    const json = await response.json(); 
-    
-    if (json.error) { 
-      dispatch({ type: "application/signin/rejected", error: json.error }); 
-    } else { 
-      dispatch({ type: "application/signin/fulfilled", payload: { json } }); 
-      localStorage.setItem("token", json.token); 
-    } 
-  }; 
-}; 
- 
-export const logOut = () => { 
-  return async (dispatch) => { 
-    dispatch({ type: "application/logOut/fulfilled" }); 
-    localStorage.clear(); 
-  }; 
+    case "application/signup/fulfilled":
+      return {
+        ...state,
+        signingUp: false,
+        user: action.payload,
+        check: true,
+      };
+
+    case "application/signup/rejected":
+      return {
+        ...state,
+        signingUp: false,
+        error: action.error,
+        check: false,
+      };
+
+    case "application/signin/pending":
+      return {
+        ...state,
+        signingIn: true,
+        error: null,
+      };
+
+    case "application/signin/fulfilled":
+      return {
+        ...state,
+        signingIn: false,
+        token: action.payload,
+      };
+
+    case "application/signin/rejected":
+      return {
+        ...state,
+        signingIn: false,
+        error: action.error,
+      };
+    case "application/logOut/fulfilled":
+      return {
+        ...state,
+        token: null,
+      };
+    default:
+      return state;
+  }
+}
+
+export const createUser = (login, password, firstName, lastName) => {
+  return async (dispatch) => {
+    dispatch({ type: "application/signup/pending" });
+
+    const responseRegister = await fetch("http://localhost:7000/users", {
+      method: "POST",
+      body: JSON.stringify({ login, password, firstName, lastName }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    const jsonRegister = await responseRegister.json();
+
+    if (jsonRegister.error) {
+      dispatch({
+        type: "application/signup/rejected",
+        error: jsonRegister.error,
+      });
+    } else {
+      dispatch({ type: "application/signup/fulfilled", payload: jsonRegister });
+    }
+  };
+};
+
+export const authUser = (login, password) => {
+  return async (dispatch) => {
+    dispatch({ type: "application/signin/pending" });
+
+    const response = await fetch("http://localhost:7000/login", {
+      method: "POST",
+      body: JSON.stringify({ login, password }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    const json = await response.json();
+    console.log(json)
+
+    if (response.status === 401) {
+      dispatch({ type: "application/signin/rejected", error: json });
+    } else {
+      dispatch({
+        type: "application/signin/fulfilled",
+        payload: json.token,
+      });
+      localStorage.setItem("token", json.token);
+    }
+  };
+};
+
+export const logOut = () => {
+  return async (dispatch) => {
+    dispatch({ type: "application/logOut/fulfilled" });
+    localStorage.clear();
+  };
 };
