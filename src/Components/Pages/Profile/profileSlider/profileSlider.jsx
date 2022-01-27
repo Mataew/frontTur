b0bb59e9001c8a86068aec7d7856dcfd8b0dfdb7
Ibragim <mih230903@gmail.com>
2022-01-27 14:01:03 +0300

    профиль делаю

--- src/Components/Pages/Profile/profileSlider/profileSlider.jsx
+++ src/Components/Pages/Profile/profileSlider/profileSlider.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./profileSlider.module.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import surt from "../../../../assets/slide1.jpg";
 import surt2 from "../../../../assets/slide3.jpg";
+import { useDispatch } from "react-redux";
+import { cartLoad } from "../../../../redux/features/profileReducer";
+import { useSelector } from "react-redux";
 
 const ProfileSlider = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(cartLoad());
+  }, [dispatch]);
+
+  const cart_Obj = useSelector((state) => state.profReducer.carts);
+
+  const turs = useSelector((state) => state.turReducer.turs);
+
+  const tur = turs.find((item) => item._id === cart_Obj.tur);
+  {turs.map((tur) => {
+    return cart_Obj.tur.map((cartTur) => {
+      if (tur._id === cartTur) {
+         return tur
+      }
+    });
+  })}
+
   return (
     <div>
       <Carousel
@@ -14,10 +35,7 @@ const ProfileSlider = () => {
         showStatus={false}
         className={styles.Profile_carousel_wrapper}
       >
-        <img src={surt} className={styles.Profile__carousel_1} />
-        <img src={surt2} className={styles.Profile__carousel_1} />
-        <div className={styles.Profile__carousel_2}>item_2</div>
-        <div className={styles.Profile__carousel_3}>item_3</div>
+{tur.from}
       </Carousel>
     </div>
   );
--- src/redux/features/profileReducer.js
+++ src/redux/features/profileReducer.js
@@ -2,7 +2,7 @@ const initialState = {
   user: [],
   error: null,
   loading: false,
-  carts: []
+  carts: {}
 };
 
 export default function profReducer (state = initialState, action) {
@@ -15,8 +15,13 @@ export default function profReducer (state = initialState, action) {
         case 'cart/postCart/fulfilled':
       return {
         ...state,
-          carts: [action.payload],
+          carts: action.payload,
       };
+      case "cart/loadCart/fulfilled":
+        return {
+          ...state,
+          carts: action.payload
+        }
     default:
       return state;
   }
@@ -56,3 +61,22 @@ export const BuyTur = (tur) => {
     }
   };
 };
+
+export const cartLoad = () => {
+  return async (dispatch) => {
+try {
+  const response = await fetch("http://localhost:7000/cart", {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    }
+  })
+  const data = await response.json()
+  dispatch({type: "cart/loadCart/fulfilled", payload: data})
+} catch (error) {
+  console.log(error.message);
+}
+  }
+}
+
