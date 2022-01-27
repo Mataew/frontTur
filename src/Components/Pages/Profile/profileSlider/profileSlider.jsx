b2af35cad8436d07b6c52a4e9c99193c3924ddac
Ibragim <mih230903@gmail.com>
2022-01-27 16:19:57 +0300

    prof

--- src/Components/Pages/Profile/profileSlider/profileSlider.jsx
+++ src/Components/Pages/Profile/profileSlider/profileSlider.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import styles from "./profileSlider.module.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import surt from "../../../../assets/slide1.jpg";
+import surt2 from "../../../../assets/slide3.jpg";
 import { useDispatch } from "react-redux";
 import { cartLoad } from "../../../../redux/features/profileReducer";
 import { useSelector } from "react-redux";
@@ -33,7 +35,7 @@ const ProfileSlider = () => {
         showStatus={false}
         className={styles.Profile_carousel_wrapper}
       >
-        
+{tur.from}
       </Carousel>
     </div>
   );
